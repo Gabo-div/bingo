1393e0d69641ab4abcdecf04e841cbfbee86805c
Gabriel Vallés <hernandezvallesgabriel@gmail.com>
2025-08-08 16:18:52 -0400

    feat(apps/server): game board functions

---
+++ apps/server/internal/game/board.go
@@ -0,0 +1,140 @@
+package game
+
+import (
+	"math/rand"
+	"slices"
+	"time"
+)
+
+func GenerateBoard(seed int64) [][]int {
+	var source rand.Source
+
+	if seed == 0 {
+		source = rand.NewSource(time.Now().UnixNano())
+	} else {
+		source = rand.NewSource(seed)
+	}
+
+	r := rand.New(source)
+
+	columnsRanges := map[string]struct {
+		start int
+		end   int
+	}{
+		"B": {1, 15},
+		"I": {16, 30},
+		"N": {31, 45},
+		"G": {46, 60},
+		"O": {61, 75},
+	}
+
+	columnsLetters := []string{"B", "I", "N", "G", "O"}
+
+	board := [][]int{
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+	}
+
+	for i, letra := range columnsLetters {
+		rangoInfo := columnsRanges[letra]
+
+		numbers := make([]int, 0, rangoInfo.end-rangoInfo.start+1)
+		for j := rangoInfo.start; j <= rangoInfo.end; j++ {
+			numbers = append(numbers, j)
+		}
+
+		r.Shuffle(len(numbers), func(k, l int) {
+			numbers[k], numbers[l] = numbers[l], numbers[k]
+		})
+
+		board[i] = numbers[:5]
+	}
+
+	finalBoard := make([][]int, 5)
+	for i := range finalBoard {
+		finalBoard[i] = make([]int, 5)
+		for j := range finalBoard[0] {
+			finalBoard[i][j] = board[j][i]
+		}
+	}
+
+	finalBoard[2][2] = 0
+
+	return finalBoard
+}
+
+func CheckHorizontalWin(board [][]int, numbers []int) bool {
+	for j := range 5 {
+
+		win := true
+
+		for i := range 5 {
+
+			n := board[j][i]
+
+			if n != 0 && !slices.Contains(numbers, n) {
+				win = false
+			}
+		}
+
+		if win {
+			return true
+		}
+	}
+
+	return false
+}
+
+func CheckVerticalWin(board [][]int, numbers []int) bool {
+	for i := range 5 {
+		win := true
+
+		for j := range 5 {
+			n := board[j][i]
+
+			if n != 0 && !slices.Contains(numbers, n) {
+				win = false
+			}
+		}
+
+		if win {
+			return true
+		}
+	}
+
+	return false
+}
+
+func CheckLeftDiagonalWin(board [][]int, numbers []int) bool {
+	for i := range 5 {
+		n := board[i][i]
+
+		if n != 0 && !slices.Contains(numbers, n) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func CheckRightDiagonalWin(board [][]int, numbers []int) bool {
+	for i := range 5 {
+		n := board[i][len(board)-1-i]
+
+		if n != 0 && !slices.Contains(numbers, n) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func CheckWin(board [][]int, numbers []int) bool {
+	return CheckHorizontalWin(board, numbers) ||
+		CheckVerticalWin(board, numbers) ||
+		CheckLeftDiagonalWin(board, numbers) ||
+		CheckRightDiagonalWin(board, numbers)
+}
---
+++ apps/server/internal/game/board_test.go
@@ -0,0 +1,147 @@
+package game
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestGenerateRandomBoard(t *testing.T) {
+	boardA := GenerateBoard(0)
+	boardB := GenerateBoard(0)
+	equal := true
+
+	for j := range 5 {
+		for i := range 5 {
+			if boardA[j][i] != boardB[j][i] {
+				equal = false
+			}
+		}
+	}
+
+	if equal {
+		t.Errorf("does not generate a different board from random seed")
+	}
+}
+
+func TestGenerateSeedBoard(t *testing.T) {
+	source := rand.NewSource(time.Now().Unix())
+	r := rand.New(source)
+	seed := r.Int63()
+
+	boardA := GenerateBoard(seed)
+	boardB := GenerateBoard(seed)
+	equal := true
+
+	for j := range 5 {
+		for i := range 5 {
+			if boardA[j][i] != boardB[j][i] {
+				equal = false
+			}
+		}
+	}
+
+	if !equal {
+		t.Errorf("does not generate the same board for seed: %d", seed)
+	}
+}
+
+func TestCheckHorizontalWin(t *testing.T) {
+	board := [][]int{
+		{2, 16, 34, 47, 61},
+		{7, 20, 33, 41, 70},
+		{14, 25, 0, 56, 64},
+		{8, 22, 36, 59, 65},
+		{1, 26, 44, 48, 68},
+	}
+
+	if !CheckHorizontalWin(board, []int{8, 22, 36, 59, 65}) {
+		t.Errorf("does not check horizontal win condition")
+	}
+
+	if !CheckHorizontalWin(board, []int{14, 25, 56, 64}) {
+		t.Errorf("does not check horizontal win condition on middle")
+	}
+
+	if CheckHorizontalWin(board, []int{14, 25, 56}) {
+		t.Errorf("does check horizontal win condition when it should not")
+	}
+}
+
+func TestCheckVerticalWin(t *testing.T) {
+	board := [][]int{
+		{2, 16, 34, 47, 61},
+		{7, 20, 33, 41, 70},
+		{14, 25, 0, 56, 64},
+		{8, 22, 36, 59, 65},
+		{1, 26, 44, 48, 68},
+	}
+
+	if !CheckVerticalWin(board, []int{34, 33, 36, 44}) {
+		t.Errorf("does not check vertical win condition")
+	}
+
+	if !CheckVerticalWin(board, []int{47, 41, 56, 59, 48}) {
+		t.Errorf("does not check vertical win condition on middle")
+	}
+
+	if CheckVerticalWin(board, []int{47, 41, 56, 59}) {
+		t.Errorf("does check vertical win condition when it should not")
+	}
+}
+
+func TestLeftDiagonalWin(t *testing.T) {
+	board := [][]int{
+		{2, 16, 34, 47, 61},
+		{7, 20, 33, 41, 70},
+		{14, 25, 0, 56, 64},
+		{8, 22, 36, 59, 65},
+		{1, 26, 44, 48, 68},
+	}
+
+	if !CheckLeftDiagonalWin(board, []int{2, 20, 59, 68}) {
+		t.Errorf("does not check left diagonal win condition")
+	}
+
+	if CheckLeftDiagonalWin(board, []int{2, 20, 59}) {
+		t.Errorf("does check left diagonal win condition when it should not")
+	}
+}
+
+func TestRightDiagonalWin(t *testing.T) {
+	board := [][]int{
+		{2, 16, 34, 47, 61},
+		{7, 20, 33, 41, 70},
+		{14, 25, 0, 56, 64},
+		{8, 22, 36, 59, 65},
+		{1, 26, 44, 48, 68},
+	}
+
+	if !CheckRightDiagonalWin(board, []int{61, 41, 22, 1}) {
+		t.Errorf("does not check right diagonal win condition")
+	}
+
+	if CheckRightDiagonalWin(board, []int{61, 41, 22}) {
+		t.Errorf("does check right diagonal win condition when it should not")
+	}
+}
+
+func BenchmarkGenerateBoard(t *testing.B) {
+	for t.Loop() {
+		GenerateBoard(0)
+	}
+}
+
+func BenchmarkCheckWin(t *testing.B) {
+	board := [][]int{
+		{2, 16, 34, 47, 61},
+		{7, 20, 33, 41, 70},
+		{14, 25, 0, 56, 64},
+		{8, 22, 36, 59, 65},
+		{1, 26, 44, 48, 68},
+	}
+
+	for t.Loop() {
+		CheckWin(board, []int{100, 101, 102, 103, 104, 105, 106, 107})
+	}
+}
